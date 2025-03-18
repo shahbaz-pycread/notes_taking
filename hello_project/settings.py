2f40d0ddd8d4e4ea599abd1df397d91c492be3bc
= <=>
2025-03-18 22:09:21 +0530

    Added allowed host

--- hello_project/settings.py
+++ hello_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('MY_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('MY_DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
